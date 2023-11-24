d41978535fef63dabc6b7895722e1e7df47a4d0b
kaiismith <ThangDD.BI12-406@st.usth.edu.vn>
2023-11-24 17:22:51 +0700

    GP044 - SIUUUUUU Real-Time Reviewed

---
+++ deploy.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+
+class GP038:
+    def __init__(self):
+        try:
+            self.__model = tf.keras.models.load_model('GP038.keras')
+            print("Model loaded")
+            self.__model.summary()
+        except:
+            print("Load error")
+    
+    def predCheat(self, data):
+        data = tf.convert_to_tensor(np.array(data), dtype=tf.float32)
+        prediction = self.__model.predict(data)
+        
+        return prediction
